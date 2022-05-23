b1c837cf27b467300cf1c7135ffd41f6029c5acd
malamud3 <@amir_malamud>
2022-05-23 10:46:48 +0300

    Initial Commit

---
+++ Quizz_game_IOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quizz_game_IOS
+//
+//  Created by Amir Malamud on 23/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
